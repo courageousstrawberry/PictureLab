25d1233a9dfd91e0090fd21ec02687e4800b4f67
courageousstrawberry <92058334+courageousstrawberry@users.noreply.github.com>
2023-07-13 16:52:56 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,16 @@
+class Main {
+  public static void main(String[] args) {
+    
+    //Activity 0
+    //ColorChooser.main();
+
+    //Activity 1
+    //PictureTester.testZeroBlue();
+    PictureTester.main();
+   
+
+
+
+
+  }
+}
